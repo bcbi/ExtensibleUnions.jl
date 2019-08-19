187898d9a832cf8c314ac720a541e54b64d12fc5
Dilum Aluthge <dilum@aluthge.com>
2019-08-19 18:16:54 -0400

    Progress commit

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,19 @@ using ExtensibleUnions
 using Test
 
 @testset "ExtensibleUnions.jl" begin
+    @testset "extensibleunion!" begin
+        module extensibleunion
+        abstract type A
+        @test_throws ArgumentError extensibleunion!(A)
+        @test_throws ArgumentError extensibleunion!(Int)
+        struct B
+            x
+        end
+        @test_throws ArgumentError extensibleunion!(B)
+        mutable struct C end
+        @test_throws ArgumentError extensibleunion!(C)
+        end
+        struct D end
+        @test_throws ArgumentError extensibleunion!(D)
+    end
 end
