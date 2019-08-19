999bb2509d819fe7f420e96cc9b20ad427769c5d
Dilum Aluthge <dilum@aluthge.com>
2019-08-19 18:45:20 -0400

    Add some more functions

--- src/ExtensibleUnions.jl
+++ src/ExtensibleUnions.jl
@@ -3,6 +3,7 @@ module ExtensibleUnions
 export extensibleunion!, extensiblefunction!, addtounion!
 
 const extensibleunionregistry = Dict{Any, Any}()
+const extensiblefunctionregistry = Dict{Any, Any}()
 
 function extensibleunion!(@nospecialize(u))
     global extensibleunionregistry
@@ -18,13 +19,64 @@ function extensibleunion!(@nospecialize(u))
     if !isimmutable(u())
         throw(ArgumentError("The provided type must be an immutable type."))
     end
+    if !haskey(extensibleunionregistry, u)
+        extensibleunionregistry[u] = Set{Any}()
+    end
+    return u
 end
 
-# function extensiblefunction!()
-# end
+function isextensibleunion(@nospecialize(u))
+    global extensibleunionregistry
+    return haskey(extensibleunionregistry, u)
+end
 
-# function addtounion!()
-# end
+function extensiblefunction!(@nospecialize(f::Function), varargs...)
+    return extensiblefunction(f, varargs)
+end
+
+function extensiblefunction!(@nospecialize(f::Function), @nospecialize(varargs::Tuple))
+    global extensiblefunctionregistry
+    if !haskey(extensiblefunctionregistry, f)
+        extensiblefunctionregistry[f] = Set{Any}()
+    end
+    for i = 1:length(varargs)
+        if isextensibleunion(varargs[i])
+            push!(extensiblefunctionregistry[f], varargs[i])
+        else
+            throw(ArgumentError("Argument is not a registered extensible union."))
+        end
+    end
+    return f
+end
+
+function isextensiblefunction(@nospecialize(f::Function))
+    global extensiblefunctionregistry
+    return haskey(extensiblefunctionregistry, f)
+end
+
+function addtounion!(@nospecialize(u), varargs...)
+    return addtounion!(f, varargs)
+end
+
+function addtounion!(@nospecialize(u), @nospecialize(varargs::Tuple))
+    global extensibleunionregistry
+    if isextensibleunion(u)
+        for i = 1:length(varargs)
+            push!(extensibleunionregistry[u], varargs[i])
+        end
+    else
+        throw(ArgumentError("First argument must be a registered extensible union."))
+    end
+end
+
+function unioncontains(@nospecialize(u), @nospecialize(t))
+    global extensibleunionregistry
+    if isextensibleunion(u)
+        return t in extensibleunionregistry[u]
+    else
+        throw(ArgumentError("First argument must be a registered extensible union."))
+    end
+end
 
 # function jl_method_def(argdata::Core.SimpleVector, codeinfo::Core.CodeInfo, mod::Module)
 #     ccall(:jl_method_def,
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,21 +3,43 @@ using Test
 
 @testset "ExtensibleUnions.jl" begin
     @testset "extensibleunion!" begin
-        abstract type A
-        end
-        @test_throws ArgumentError extensibleunion!(A)
+        abstract type A1 end
+        @test_throws ArgumentError extensibleunion!(A1)
         @test_throws ArgumentError extensibleunion!(Int)
-        struct B
+        struct S1
             x
         end
-        @test_throws ArgumentError extensibleunion!(B)
-        mutable struct C
-        end
-        @test_throws ArgumentError extensibleunion!(C)
-        struct D
-        end
-        extensibleunion!(D)
-        extensibleunion!(D)
-        extensibleunion!(D)
+        @test_throws ArgumentError extensibleunion!(S1)
+        mutable struct S2 end
+        @test_throws ArgumentError extensibleunion!(S2)
+        struct S3 end
+        @test !ExtensibleUnions.isextensibleunion(S3)
+        extensibleunion!(S3)
+        @test ExtensibleUnions.isextensibleunion(S3)
+        extensibleunion!(S3)
+        @test ExtensibleUnions.isextensibleunion(S3)
+        extensibleunion!(S3)
+        @test ExtensibleUnions.isextensibleunion(S3)
+    end
+    @testset "extensiblefunction!" begin
+        struct S4
+        function f1 end
+        @test !ExtensibleUnions.isextensibleunion(S4)
+        @test !ExtensibleUnions.isextensiblefunction(f1)
+        @test_throws ArgumentError extensiblefunction!(f1, S4)
+        @test !ExtensibleUnions.isextensibleunion(S4)
+        @test !ExtensibleUnions.isextensiblefunction(f1)
+        extensibleunion!(S4)
+        @test ExtensibleUnions.isextensibleunion(S4)
+        extensiblefunction!(f1, S4)
+        @test ExtensibleUnions.isextensiblefunction(f1)
+        extensiblefunction!(f1, S4)
+        @test ExtensibleUnions.isextensiblefunction(f1)
+        extensiblefunction!(f1, S4)
+        @test ExtensibleUnions.isextensiblefunction(f1)
+    end
+    @testset "addtounion!" begin
+    end
+    @testset "unioncontains" begin
     end
 end
