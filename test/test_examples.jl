626b8e517e55f4f3bfad19a72cb80f699f59d26a
Dilum Aluthge <dilum@aluthge.com>
2019-08-19 21:57:16 -0400

    Progress commit

--- test/test_examples.jl
+++ test/test_examples.jl
@@ -2,57 +2,68 @@ using ExtensibleUnions
 using Test
 
 abstract type AbstractCar end
-abstract type AbstractMinivan end
-abstract type AbstractSUV end
 abstract type AbstractFireEngine end
+
 struct RedCar <: AbstractCar
 end
 struct BlueCar <: AbstractCar
     x
 end
-mutable struct RedMinivan <: AbstractMinivan
-end
-mutable struct BlueMinivan <: AbstractMinivan
-    x
-end
-struct RedSUV{T} <: AbstractSUV
-    x::T
-end
-struct BlueSUV{T} <: AbstractSUV
-    x::T
-    y::T
-end
-mutable struct LadderTruck{T} <: AbstractFireEngine
+
+struct LadderTruck{T} <: AbstractFireEngine
     x::T
 end
 mutable struct WaterTender{T} <: AbstractFireEngine
     x::T
     y::T
 end
+
 struct RedColorTrait end
 struct BlueColorTrait end
+
 extensibleunion!(RedColorTrait)
 extensibleunion!(BlueColorTrait)
-describe(x) = "I don't know what this is"
-describe(RedCar())
-describe(BlueCar(1))
-describe(LadderTruck{Int}(1))
-describe(x::RedColorTrait) = "The color of this object is red"
-extensiblefunction!(describe, RedColorTrait)
-describe(x::BlueColorTrait) = "The color of this object is blue"
-extensiblefunction!(describe, BlueColorTrait)
+
+describe(x) = "I don't know anything about this object"
+
+methods(describe)
 describe(RedCar())
 describe(BlueCar(1))
 describe(LadderTruck{Int}(2))
+describe(WaterTender{Int}(3,4))
+
+describe(x::RedColorTrait) = "The color of this object is red"
+extensiblefunction!(describe, RedColorTrait)
+
+describe(x::BlueColorTrait) = "The color of this object is blue"
+extensiblefunction!(describe, BlueColorTrait)
+
+methods(describe)
+describe(RedCar())
+describe(BlueCar(1))
+describe(LadderTruck{Int}(2))
+describe(WaterTender{Int}(3,4))
 
 addtounion!(RedColorTrait, RedCar)
 
-
-
+methods(describe)
 describe(RedCar())
 describe(BlueCar(1))
 describe(LadderTruck{Int}(2))
+describe(WaterTender{Int}(3,4))
+
+addtounion!(BlueColorTrait, BlueCar)
+
+methods(describe)
+describe(RedCar())
+describe(BlueCar(1))
+describe(LadderTruck{Int}(2))
+describe(WaterTender{Int}(3,4))
+
 addtounion!(RedColorTrait, AbstractFireEngine)
+
+methods(describe)
 describe(RedCar())
 describe(BlueCar(1))
 describe(LadderTruck{Int}(2))
+describe(WaterTender{Int}(3,4))
