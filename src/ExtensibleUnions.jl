5c861ff61ae933101a2e479cadcb692876bd76b3
Dilum Aluthge <dilum@aluthge.com>
2019-08-23 16:04:47 -0400

    Update ExtensibleUnions.jl

--- src/ExtensibleUnions.jl
+++ src/ExtensibleUnions.jl
@@ -4,7 +4,7 @@ export addtounion!, # extensible_unions.jl
        extensiblefunction!, # extensible_functions.jl
        extensibleunion!, # extensible_unions.jl
        isextensiblefunction, # extensible_functions.jl
-       isextensibleunion, # extensible_unions.jl
+       isextensibleunion # extensible_unions.jl
 
 const _registry_extensibleunion_to_genericfunctions = Dict{Any, Any}()
 const _registry_extensibleunion_to_members = Dict{Any, Any}()
